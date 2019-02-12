caa29618feee8291a2440fecc8e2019e0f12b77d
Fernando <fernandodlv32@gmail.com>
2019-02-12 18:17:14 -0500

    added to readme

--- src/index.php
+++ src/index.php
@@ -10,7 +10,7 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <style>
-      <?php include('./index.css')?>
+      <?php include('./styles/index.css')?>
     </style>
     <body>
       <?php 
